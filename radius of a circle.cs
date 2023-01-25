43bdec344ef895f1e5d2302202d88cf98d3bb234
E-INK <99248147+E-INK@users.noreply.github.com>
2023-01-25 02:50:09 -0800

    Add files via upload
    the program calculates the area of a circle once the radius is inserted

---
+++ radius of a circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace ConsoleApplication
+{
+    class areaOfACircle 
+    {
+        static void formula()
+        {               //initialisation, input and declaration of variables;
+            Console.WriteLine("=========================================================================================");
+            Console.WriteLine("============================AREA OF A CIRCLE=============================================");
+            Console.WriteLine("=========================================================================================");
+            Console.WriteLine("ENTER RADIUS OF THE CIRCLE:");
+            const double pi = 3.14;  
+            double radius = Convert.ToDouble(Console.ReadLine());
+            double answer = pi * radius * radius;
+            Console.WriteLine("The area of your circle is" + " " + answer + " " + "cm\xB2");
+           
+        }
+        static void Main(string[] args)
+        {
+            
+            formula();                      //method is executed here;
+            Console.ReadKey();
+        }
+    }
+}
